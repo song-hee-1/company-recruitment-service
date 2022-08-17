eb4c58cc580c694ff74291bfcd7ecbd6c7a34d5f
한송희 <98_han@naver.com>
2022-08-17 16:18:25 +0900

    ✨ Feat : create serializer to change json data

---
+++ jobpostings/serializers.py
@@ -0,0 +1,66 @@
+from rest_framework import serializers
+from django.db.models import Q
+
+from .models import User, Company, Jobposting, Apply
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        exclude = ['create_time', 'update_time']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('user_id', 'password', 'username', 'email')
+        extra_kwargs = {"password": {"write_only": True}}
+
+
+# 채용공고 전체 목록
+class JobpostingSerializer(serializers.ModelSerializer):
+    company_name = serializers.ReadOnlyField(source='company.name')
+    country = serializers.ReadOnlyField(source='company.country')
+    region = serializers.ReadOnlyField(source='company.region')
+
+    class Meta:
+        model = Jobposting
+        exclude = ['create_time', 'update_time']
+
+
+# 채용공고 등록
+class JobpostingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Jobposting
+        exclude = ['create_time', 'update_time']
+
+
+# 채용공고 수정
+class JobpostingUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Jobposting
+        exclude = ['create_time', 'update_time']
+
+
+# 채용공고 상세정보
+class JobpostingDetailSerializer(serializers.ModelSerializer):
+    company_name = serializers.ReadOnlyField(source='company.name')
+    country = serializers.ReadOnlyField(source='company.country')
+    region = serializers.ReadOnlyField(source='company.region')
+    other_post = serializers.SerializerMethodField(method_name='get_other_post')
+
+    def get_other_post(self, obj):
+        posts = Jobposting.objects.filter(~Q(jobposting_id=obj.jobposting_id), company_id=obj.company_id)
+        posts_id = [post.jobposting_id for post in posts]
+        return posts_id
+
+    class Meta:
+        model = Jobposting
+        exclude = ['create_time', 'update_time']
+
+
+# 채용공고 지원
+class ApplymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Apply
+        exclude = ['create_time', 'update_time']
